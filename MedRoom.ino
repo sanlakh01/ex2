250823e5587d9f4acb696456c2397874a9505b85
sanlakh01 <122475458+sanlakh01@users.noreply.github.com>
2024-02-27 01:06:04 -0500

    Add files via upload

--- MedRoom.ino
+++ MedRoom.ino
@@ -1,13 +1,13 @@
 int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
 int photocellReading;     // the analog reading from the sensor divider
-int bright;
-const int rgb_red = 11;
-const int rgb_blue = 3;
-const int rgb_green = 5;
+int bright;               //the brightness value mapped from photoresistor input
+const int rgb_red = 11;   //RGB LED output pin
+const int rgb_blue = 3;   //RGB LED output pin
+const int rgb_green = 5;   //RGB LED output pin
 
-int redVal = 0;
-int greenVal = 0;
-int blueVal = 0;
+int redVal = 0;     //value to be sent to serial monitor
+int greenVal = 0;   //value to be sent to serial monitor
+int blueVal = 0;    //value to be sent to serial monitor
 
 void setup() {
   // We'll send debugging information via the Serial monitor
@@ -18,6 +18,7 @@ void setup() {
   pinMode(rgb_blue, OUTPUT); // Sets the blue pin as an Output
 }
 
+//this function sets the RGB LED color from Web Serial
 void setRGBLedColor(int red, int green, int blue){
   analogWrite(rgb_red, red);
   analogWrite(rgb_green, green);
@@ -26,13 +27,12 @@ void setRGBLedColor(int red, int green, int blue){
 
 void loop() {
   
-  photocellReading = analogRead(photocellPin);
-  photocellReading = 1023 - photocellReading;
-  //now we have to map 0-1023 to 0-255 since thats the range analogWrite uses
-  bright = map(photocellReading, 0, 1000, 0, 100); 
-  Serial.println(bright);
+  photocellReading = analogRead(photocellPin);  //input from photoresistor
+  photocellReading = 1023 - photocellReading;   //reversing it, since higher the reading, the dimmer the LED
+  bright = map(photocellReading, 0, 1000, 0, 100); //mapping the reading to a "brightness ratio" --> see document for explanation
+  Serial.println(bright); //to the serial it goes! (so that the webpage can also "dim" or "brighten" accordingly)
 
-  // Read color data from the serial
+  // Read color data from the serial - following code taken directly from "UltraRipple Sensor" Demo files
   if(Serial.available() > 0){
     // If we are here, then serial data has been received
     // Read data off the serial port until we get ot the endline delimeter ('\n')
@@ -64,5 +64,7 @@ void loop() {
       setRGBLedColor(redVal, greenVal, blueVal);
     }
   }
-  delay(1000);
+  delay(1000); //setting delay to slow down the rapid changes in LED color
 }
+//Code sourced from in-class example code and from Arduino Stack Exchange. 
+//https://arduino.stackexchange.com/questions/86427/how-to-change-brightness-of-led-light
